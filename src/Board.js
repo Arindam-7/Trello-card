07929f39e98a150f5a5f69e99706bfa2544b6b0d
Arindam <msaha6368@gmail.com>
2020-11-17 17:12:37 +0530

    column component renders effectively now

--- src/Board.js
+++ src/Board.js
@@ -4,54 +4,58 @@ import React from 'react';
 
 
 const Board = () => {
-  const tasks = [
-    {
+  const todo = {
+    todo1: {
       id: 1,
       name: 'Learn about API',
-      type: 'todo'
+      type: 'To Do'
     },
-    {
+    todo2: {
       id: 2,
       name: 'Create a roadmap for learning Java',
-      type: 'todo'
+      type: 'To Do'
     },
-    {
+    todo3: {
       id: 3,
       name: 'Watch Peaky Blinders',
-      type: 'todo'
-    },
-    {
+      type: 'To Do'
+    }
+  }
+
+  const doing = {
+    doing1: {
       id: 4,
       name: 'Trello Card Project',
-      type: 'doing'
+      type: 'Doing'
     },
-    {
+    doing2: {
       id: 5,
       name: 'Listen to music',
-      type: 'doing'
-    },
-    {
+      type: 'Doing'
+    }
+  }
+
+  const done = {
+    done1: {
       id: 6,
       name: 'Practice Touch Typing',
-      type: 'done'
+      type: 'Done',
     },
-    {
+    done2: {
       id: 7,
       name: 'Complete Freelance work',
-      type: 'done'
+      type: 'Done',
     },
-    {
+    done3: {
       id: 8,
       name: "Transfer money to bank account",
-      type: 'done'
-    }
-  ]
+      type: 'Done',
+    },
+  }
 
   return (
     <div className="Board">
-      <Column />
-      <Column />
-      <Column />
+      <Column todo={todo} doing={doing} done={done} />
     </div>
   );
 }
--- src/Components/Column.js
+++ src/Components/Column.js
@@ -2,20 +2,45 @@ import React from 'react';
 import './Column.css';
 
 
-const Column = () => {
+const Column = (props) => {
     return (
+    <React.Fragment>
         <div className="column">
-            <h1 className="title"> To Do </h1>
+            <h1 className="title">{props.todo.todo1.type}</h1>
             <div class="item">
-                <h2 className="item-title">Do Something</h2>
+                <h2 className="item-title">{props.todo.todo1.name}</h2>
             </div>
             <div class="item">
-                <h2 className="item-title">Do Something(2)</h2>
+                <h2 className="item-title">{props.todo.todo2.name}</h2>
             </div>
             <div class="item">
-                <h2 className="item-title">Do Something(3)</h2>
+                <h2 className="item-title">{props.todo.todo3.name}</h2>
             </div>
         </div>
+
+        <div className="column">
+            <h1 className="title">{props.doing.doing1.type}</h1>
+            <div class="item">
+                <h2 className="item-title">{props.doing.doing1.name}</h2>
+            </div>
+            <div class="item">
+                <h2 className="item-title">{props.doing.doing2.name}</h2>
+            </div>
+        </div>
+
+        <div className="column">
+            <h1 className="title">{props.done.done1.type}</h1>
+            <div class="item">
+                <h2 className="item-title">{props.done.done1.name}</h2>
+            </div>
+            <div class="item">
+                <h2 className="item-title">{props.done.done2.name}</h2>
+            </div>
+            <div class="item">
+                <h2 className="item-title">{props.done.done3.name}</h2>
+            </div>
+        </div>
+    </React.Fragment>
     );
 }
 
